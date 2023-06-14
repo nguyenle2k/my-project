f51097a64453c09ffb7ef67600969cdba14708b1
NguyênLê2k <nle372999@gmail.com>
2023-06-14 08:38:57 +0700

    fix(home): delete cmd

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,19 +5,11 @@ import { map } from 'rxjs/operators';
 import { loadItems } from 'src/app/ngrx-test/items.actions';
 import { selectItems } from 'src/app/ngrx-test/items.selectors';
 import { Observable } from 'rxjs';
-import { AppState } from 'src/app/ngrx-test/app.state';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  items$: Observable<any[]>;
-
-  constructor(private store: Store<AppState>) {
-    this.items$ = this.store.pipe(
-      select(selectItems),
-      map((items: any[]) => items)
-    );
-  }
+  constructor() {}
 }
